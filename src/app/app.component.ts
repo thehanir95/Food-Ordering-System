f85978b2ca9849b80f2a23ed9ef5762efd568aba
thehanir95 <thehanicis95@gmail.com>
2020-04-12 22:40:32 +0530

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pambahinna-Food-Delivery';
+
+  
 }
 
 
--- src/app/cabin/cabin.component.ts
+++ src/app/cabin/cabin.component.ts
@@ -7,28 +7,33 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./cabin.component.scss']
 })
 export class CabinComponent implements OnInit {
+  
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+
   foodcabinname=new FormControl();
   ownername=new FormControl();
   mobileno=new FormControl();
-  email=new FormControl();
   address=new FormControl();
   password=new FormControl();
   confirmpassword=new FormControl();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
 
   submit(){
     alert(this.foodcabinname.value);
     alert(this.ownername.value);
     alert(this.mobileno.value);
-    alert(this.email.value);
     alert(this.address.value);
     alert(this.password.value);
     alert(this.confirmpassword.value);
-    
+   
   }
+  
 
 }
